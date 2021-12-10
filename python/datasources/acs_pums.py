541719daabc154ec1b3ee9d8f4c257ace27b31ad
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-12-10 15:50:24 -0500

    more changes

--- python/datasources/acs_pums.py
+++ python/datasources/acs_pums.py
@@ -3,6 +3,8 @@ import json
 from ingestion.gcs_to_bq_util import values_json_to_dataframe
 from ingestion.standardized_columns import Race
 
+import pandas as pd
+
 
 NOT_HISPANIC_IDENTIFIER = "01"
 RACE_IDENTIFIERS = {
@@ -12,40 +14,36 @@ RACE_IDENTIFIERS = {
     Race.ASIAN_NH.value: (6, ),
     Race.NHPI_NH.value: (7, ),
     Race.MULTI_OR_OTHER_STANDARD_NH.value: (8, 9),
+    Race.WHITE.value: (1, ),
+    Race.BLACK.value: (2, ),
+    Race.AIAN.value: (3, 4, 5),
+    Race.ASIAN.value: (6, ),
+    Race.NHPI.value: (7, ),
+    Race.MULTI_OR_OTHER_STANDARD.value: (8, 9),
 }
 
-RACES = [
-    Race.WHITE_NH.value,
-    Race.BLACK_NH.value,
-    Race.AIAN_NH.value,
-    Race.ASIAN_NH.value,
-    Race.NHPI_NH.value,
-    Race.MULTI_OR_OTHER_STANDARD_NH.value,
-]
 
-AGE_RANGES = ["0:9", "10:19", "20:29", "30:39", "40:49", "50:59", "60:69", "70:79", "80:120"]
+AGE_RANGES = ["0:9", "10:19", "20:29", "30:39", "45:54", "55:64", "65:74", "75:84", "85:120"]
 
 BASE_ACS_URL = "https://api.census.gov/data/2019/acs/acs5/pums"
 
 
 def get_state_population_data():
-    url = ("https://api.census.gov/data/2019/acs/acs5/pums?"
-           "tabulate=weight(PWGTP)&col+RAC1P&row+HISP&row+ucgid&ucgid=0400000US04&AGEP=10:20")
+    dfs = {}
 
-    jsn = requests.get(url)
-    jsn = json.loads(jsn.content)
-    jsn[0] = [str(item) for item in jsn[0]]
+    for age_range in AGE_RANGES:
+        jsn = requests.get(generate_url(age_range))
+        jsn = json.loads(jsn.content)
 
-    df = values_json_to_dataframe(json.dumps(jsn))
+        jsn[0] = [str(item) for item in jsn[0]]
+        dfs[age_range] = values_json_to_dataframe(json.dumps(jsn))
 
-    return df
+    return dfs
 
 
-def generate_params(state, age_range):
-    params = {}
-    params["tabluate"] = "weight(PWGTP)&col+RAC1P&row+HISP&row+ucgid"
-    params["ucgid"] = "0400000US04"
-    params["AGEP"] = age_range
+def generate_url(age_range):
+    return "%s?tabulate=weight(PWGTP)&col+RAC1P&row+HISP&row+ucgid&ucgid=0400000US04&AGEP=%s" % (
+            BASE_ACS_URL, age_range)
 
 
 def get_race_keys(races):
@@ -54,7 +52,7 @@ def get_race_keys(races):
         for pum_num in RACE_IDENTIFIERS[race]:
             keys.append(str({"RAC1P": str(pum_num)}))
 
-    return keys
+    return list(set(keys))
 
 
 def get_non_hispanic_population_for_race(df, race):
@@ -74,13 +72,64 @@ def get_hispanic_population(df):
     return int(df.sum().sum())
 
 
+def translate_age(age_range):
+    if age_range == "85:120":
+        return "85+"
+    return age_range.replace(':', '-')
+
+
 def get_all_population_data():
-    populations = {}
-    df = get_state_population_data()
+    populations_by_age = []
+    dfs = get_state_population_data()
 
-    for race in RACE_IDENTIFIERS:
-        populations[race] = get_non_hispanic_population_for_race(df, race)
+    for ages, df in dfs.items():
+        for race in RACE_IDENTIFIERS:
+            if "NH" in race:
+                populations = {}
+                populations['race'] = race
+                populations['population'] = get_non_hispanic_population_for_race(df, race)
+                populations['age'] = translate_age(ages)
+                populations_by_age.append(populations)
 
-    populations[Race.HISP.value] = get_hispanic_population(df)
+            else:
+                populations = {}
+                populations['race'] = race
+                populations['population'] = get_total_population_for_race(df, race)
+                populations['age'] = translate_age(ages)
+                populations_by_age.append(populations)
 
-    return populations
+        populations = {}
+        populations['race'] = Race.HISP.value
+        populations['population'] = get_hispanic_population(df)
+        populations['age'] = translate_age(ages)
+
+        populations_by_age.append(populations)
+
+    return pd.DataFrame(populations_by_age)
+
+
+def compare_to_estimate():
+    estimate_df = pd.read_json("az.json")
+    df = get_all_population_data()
+
+    all_differences = []
+    for ages in AGE_RANGES:
+        age_range = translate_age(ages)
+
+        estimate_df_age = estimate_df.loc[estimate_df['age'] == age_range]
+        df_age = df.loc[df['age'] == age_range]
+
+        for race in RACE_IDENTIFIERS.keys() + [Race.HISP.value]:
+            differences = {}
+
+            if len(estimate_df_age.loc[estimate_df_age['race_category_id'] == race]) > 0:
+
+                est = int(estimate_df_age.loc[estimate_df_age['race_category_id'] == race]['population'].values[0])
+                pums_val = int(df_age.loc[df_age['race'] == race]['population'].values[0])
+
+                differences['race'] = race
+                differences['age'] = age_range
+                differences['diff'] = float(pums_val) / float(est)
+                all_differences.append(differences)
+
+    return pd.DataFrame(all_differences)
